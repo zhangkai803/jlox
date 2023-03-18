7508ae4b0506d973920f726bfc9830d2ee205074
zk <kai.zhang@lizhiweike.com>
2023-03-18 23:54:07 +0800

    log: comment some debug log

--- com/zk/jlox/Jlox.java
+++ com/zk/jlox/Jlox.java
@@ -15,15 +15,15 @@ public class Jlox {
     private static final Interpreter interpreter = new Interpreter();
 
     public static void main(String[] args) throws IOException {
-        Expr expression = new Expr.Binary(
-        new Expr.Unary(
-            new Token(TokenType.MINUS, "-", null, 1),
-            new Expr.Literal(123)),
-        new Token(TokenType.STAR, "*", null, 1),
-        new Expr.Grouping(
-            new Expr.Literal(45.67)));
+        // Expr expression = new Expr.Binary(
+        // new Expr.Unary(
+        //     new Token(TokenType.MINUS, "-", null, 1),
+        //     new Expr.Literal(123)),
+        // new Token(TokenType.STAR, "*", null, 1),
+        // new Expr.Grouping(
+        //     new Expr.Literal(45.67)));
 
-        System.out.println(new AstPrinter().print(expression));
+        // System.out.println(new AstPrinter().print(expression));
 
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -66,9 +66,9 @@ public class Jlox {
         Scanner scanner = new Scanner(string);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token t: tokens) {
-            System.out.println(t);
-        }
+        // for (Token t: tokens) {
+        //     System.out.println(t);
+        // }
 
         // 解析器 生成语句
         Parser parser = new Parser(tokens);
